ed5923baaa139d3966ffc53bcbee07da038811a5
Yunus Emre <yunus.eminler@gmail.com>
2024-09-26 21:29:21 +0300

    first commit

---
+++ todoList/todoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoListApp.swift
+//  todoList
+//
+//  Created by Yunus Emre Eminler on 26.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct todoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
