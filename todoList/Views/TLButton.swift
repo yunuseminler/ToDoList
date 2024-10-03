894b31d7e0cab01c72fce564e56b12ff9a2fedac
Yunus Emre <yunus.eminler@gmail.com>
2024-10-04 00:10:01 +0300

    Register View Added

---
+++ todoList/Views/TLButton.swift
@@ -0,0 +1,18 @@
+//
+//  TLButton.swift
+//  todoList
+//
+//  Created by Yunus Emre Eminler on 3.10.2024.
+//
+
+import SwiftUI
+
+struct TLButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TLButton()
+}
