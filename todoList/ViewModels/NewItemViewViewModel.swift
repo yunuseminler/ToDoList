f77105014cccdfaba2d191103039ed10343fb853
Yunus Emre <yunus.eminler@gmail.com>
2024-10-08 22:48:15 +0300

    New Item Save Database done

--- todoList/ViewModels/NewItemViewViewModel.swift
+++ todoList/ViewModels/NewItemViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Yunus Emre Eminler on 26.09.2024.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class NewItemViewViewModel: ObservableObject {
@@ -13,7 +15,24 @@ class NewItemViewViewModel: ObservableObject {
     @Published var showAlert = false
     init() {}
 
-    func save() {}
+    func save() {
+        guard canSave else {
+            return
+        }
+
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(id: newId, title: title, dueDate: dueDate.timeIntervalSince1970, createDate: Date().timeIntervalSince1970, isDone: false)
+
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictionary())
+    }
 
     var canSave: Bool {
         guard !title.trimmingCharacters(in: .whitespaces).isEmpty else {
