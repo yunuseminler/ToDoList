009a9da340a59a9416d4bad406ded861af128ab8
Yunus Emre <yunus.eminler@gmail.com>
2024-10-08 21:47:49 +0300

    Main tab page and pages created

--- todoList/Views/MainView.swift
+++ todoList/Views/MainView.swift
@@ -11,9 +11,18 @@ struct MainView: View {
     @StateObject var viewModel = MainViewViewModel()
     var body: some View {
         if viewModel.isSignedIn, !viewModel.currentUserId.isEmpty {
-            ToDoListView()
+            accountView
         }
         else { LoginView() }
+    }
+
+    @ViewBuilder
+    var accountView: some View {
+        TabView {
+            ToDoListView(userId: viewModel.currentUserId).tabItem { Label("Home", systemImage: "house") }
+            ProfileView().tabItem { Label("Profile", systemImage: "person.circle")
+            }
+        }
     }
 }
 
--- todoList/Views/ToDoListView.swift
+++ todoList/Views/ToDoListView.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct ToDoListView: View {
+    @StateObject var viewModel = ToDoListViewViewModel()
+
+    private let userId: String
+    init(userId: String) {
+        self.userId = userId
+    }
+
     var body: some View {
-        Text("Logged In buddy.")
+        NavigationView {
+            VStack {}.navigationTitle("To Do List").toolbar {
+                Button {} label: {
+                    Image(systemName: "plus")
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    ToDoListView()
+    ToDoListView(userId: "")
 }
