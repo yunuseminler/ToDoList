36ca91e837d190a7877f62d3d5d9b3cc1ed0d959
Yunus Emre <yunus.eminler@gmail.com>
2024-10-11 00:00:13 +0300

    Profile Page and Logout button is done with funcionality-

--- todoList/ViewModels/ToDoListItemViewViewModel.swift
+++ todoList/ViewModels/ToDoListItemViewViewModel.swift
@@ -4,9 +4,27 @@
 //
 //  Created by Yunus Emre Eminler on 26.09.2024.
 //
-
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class ToDoListItemViewViewModel: ObservableObject {
     init() {}
+    
+    func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
+    }
 }
--- todoList/Views/ProfileView.swift
+++ todoList/Views/ProfileView.swift
@@ -8,10 +8,44 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @StateObject var viewModel = ProfileViewViewModel()
     var body: some View {
         NavigationView {
-            VStack {}.navigationTitle("Profile")
+            VStack {
+                if let user = viewModel.user {
+                    profile(user: user)
+                } else {
+                    Text("Loading ...")
+                }
+
+            }.navigationTitle("Profile")
+        }.onAppear {
+            viewModel.fetchUser()
         }
+    }
+
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        Image(systemName: "person.circle").resizable().aspectRatio(contentMode: .fit).foregroundStyle(.blue).frame(width: 125, height: 125).padding()
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Name: ").bold()
+                Text(user.name)
+            }.padding()
+            HStack {
+                Text("Email: ").bold()
+                Text(user.email)
+            }.padding()
+            HStack {
+                Text("Member Since : ").bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }.padding()
+
+        }.padding()
+        Button("Logout") {
+            viewModel.logOut()
+        }.tint(.red).padding()
+        Spacer()
     }
 }
 
